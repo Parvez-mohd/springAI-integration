4c21e9d472ecddcdbf0290954d73a38c9716f06a
Parvez-mohd <meparvez06@gmail.com>
2026-08-23 18:27:06 +0530

    fallback mechanism added

--- src/main/java/com/practice/firstspringai/controller/SampleController.java
+++ src/main/java/com/practice/firstspringai/controller/SampleController.java
@@ -3,6 +3,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.http.MediaType;
+
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -15,7 +18,8 @@ public SampleController() {
     }
 
 
-    @GetMapping("/bookData")
+    @GetMapping(value = "/bookData", produces = MediaType.APPLICATION_JSON_VALUE)
+    @CircuitBreaker(name = "openLibraryService", fallbackMethod = "getBookDataFallback")
     public String getBookData() {
         log.info("Fetching book data from Open Library API...");
         String response =  restClient.get()
@@ -24,6 +28,21 @@ public SampleController() {
                 .body(String.class);
                 log.info("Response payload length: {}", response != null ? response.length() : 0);
         return response;
+    }
+
+    public String getBookDataFallback(Throwable throwable) {
+        log.error("Open Library API is down or failing. Triggering fallback. Error: {}", throwable.getMessage());
+
+        return """
+                {
+                  "status": "Fallback",
+                  "message": "The external book service is currently unavailable. Please try again later.",
+                  "cachedData": {
+                    "title": "Default Book Title (Cached)",
+                    "isbn": "0451450523"
+                  }
+                }
+                """;
     }
     
 }
